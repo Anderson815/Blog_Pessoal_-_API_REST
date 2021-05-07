9769421124bad1b5de9527320ebed9fa19315c19
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-07 15:41:22 -0300

    feat: endpoint do usuario, livre de token

--- src/main/java/org/generation/blogpessoal/service/UsuarioService.java
+++ src/main/java/org/generation/blogpessoal/service/UsuarioService.java
@@ -1,5 +1,53 @@
 package org.generation.blogpessoal.service;
 
-public class UsuarioService {
 
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.generation.blogpessoal.model.UserLogin;
+import org.generation.blogpessoal.model.Usuario;
+import org.generation.blogpessoal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.apache.commons.codec.binary.Base64;
+
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public Usuario cadastrarUsuario(Usuario usuario) {
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+		
+		return repository.save(usuario);
+	} 
+	
+	public Optional<UserLogin> logar(Optional<UserLogin> user){
+		
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		// dar uma olhada no repository do usuario no método especial
+		Optional<Usuario> usuario = repository.findByNome(user.get().getNome());		
+		
+		if(usuario.isPresent()) {
+			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+				
+				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodeAuth);
+				
+				user.get().setToken(authHeader);
+				user.get().setNome(user.get().getNome());
+				
+				return user;
+			}
+		}
+		
+		return null;
+	}
 }
