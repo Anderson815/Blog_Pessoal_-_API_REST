d2de6ba7b0c1b91a9a7804fb17dcc9b5ffabf911
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-13 14:12:33 -0300

    docs: detalhamento das respostas de usuário no Swagger

--- src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/org/generation/blogpessoal/controller/UsuarioController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -25,18 +27,26 @@ public class UsuarioController {
 	private UsuarioService usuarioService;
 	
 	@ApiOperation(value = "Cadastra um novo usuário")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "O usuário foi cadastrado"),
+            @ApiResponse(code = 400, message = "Esse usuário já existe na plataforma")
+        })
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> post(@RequestBody Usuario usuario){
 		Optional<Usuario> user = usuarioService.cadastrarUsuario(usuario);
 		
 		try {
-			return ResponseEntity.ok(user.get());
+			return ResponseEntity.status(HttpStatus.CREATED).body(user.get());
 		}catch (Exception e) {
 			return ResponseEntity.badRequest().build();
 		}
 	}
 	
 	@ApiOperation(value = "Loga o usuário na plataforma")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 401, message = "Esse usuário não existe na plataformaa")
+        })
 	@PostMapping("/logar")
 	public ResponseEntity<UserLogin> autentication(@RequestBody Optional<UserLogin> user){
 		return usuarioService.logar(user).map(resp -> ResponseEntity.ok(resp))
