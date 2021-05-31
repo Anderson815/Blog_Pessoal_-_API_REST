5f95d53aab5e2e4a178d523575d5849c6c111320
Anderson Correia de Souza <anderson_souza33@outlook.com.br>
2021-05-31 10:36:24 -0300

    feat: foto e tipo do usuário

--- src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -46,6 +46,10 @@ public class Postagem {
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+	
 	//Getters e Setters
 	
 	public long getId() {
@@ -86,6 +90,14 @@ public class Postagem {
 
 	public void setTema(Tema tema) {
 		this.tema = tema;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 	
 }
